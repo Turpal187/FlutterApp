db3e47ec7c7ca93d303617fbf88791a2d7b571c1
Taha <Ulusoy.Taha061@hotmail.com>
2021-10-30 21:21:54 +0200

    Add Tasks View

---
+++ lib/screens/dashboard/tasks_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+
+            
+
+          ],
+        ),
+      ),
+    );
+  }
+
+}
